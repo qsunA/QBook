1ca982b57824ea40b76da15b7ce35c05c54fbbc7
QsunA <ks.aum@theenm.com>
2019-10-04 18:56:55 +0900

    register change

--- pages/myBook.js
+++ pages/myBook.js
@@ -1,9 +1,14 @@
 import BookImgListComponent from "../components/myBook/BookImgListComponent"
+import Link from "next/link";
 
 
 const myBook = ()=>{
     return(
         <div>
+            <div>
+                <Link href="./registerBook"><button>책 등록하기</button></Link>
+                <button>엑셀로 책 등록하기</button>
+            </div>
             <BookImgListComponent/>
         </div>
     )
--- pages/registerBook.js
+++ pages/registerBook.js
@@ -1,5 +1,5 @@
 import { useInput } from "../utils/utils";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const registerBook = ()=>{
 
@@ -7,7 +7,7 @@ const registerBook = ()=>{
     const [title,onChangeTitle] = useInput('');
     const [publisher, onChangePublisher] = useInput('');
     const [author, onChangeAuthor] = useInput('');
-    const [category, onChangeCategory] = useState('');
+    const [category, setCategory] = useState('');
     const [price, onChangePrice] = useInput(0);
     const [subTitle, onChangeSubTitle] =useInput('');
 
@@ -18,8 +18,36 @@ const registerBook = ()=>{
         {label: '컴퓨터', value : 'computer'}
     ];
 
+    const onChangeCategory = useCallback((e)=>{
+        setCategory(e.target.value);
+    },[]);
+
+    const onSubmit = useCallback((e) => {
+        e.preventDefault();
+
+        const book = {
+            isbn : isbn,
+            title : title,
+            publisher : publisher,
+            author : author,
+            category : category,
+            price : price,
+            subTitle : subTitle
+        };
+
+        console.log({
+            isbn,
+            title,
+            publisher,
+            author,
+            category,
+            price,
+            subTitle
+        })
+    },[isbn,title,publisher,author,category,price, subTitle]);
+
     return(
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="register-main">
                 <div className="register-form">
                     <div className="form-input">
@@ -48,7 +76,7 @@ const registerBook = ()=>{
                     </div>
                     <div className="form-input">
                         <label htmlFor="book-category">분야</label>
-                        <select>
+                        <select value={category} onChange={onChangeCategory}>
                             {
                                 categories.map((v,i)=>
                                     <option value={v.value} key={i}>{v.label}</option>
@@ -60,6 +88,9 @@ const registerBook = ()=>{
                 <div className="img-btn">
                     <button>이미지 등록</button>
                 </div>
+            </div>
+            <div className="btn-div">
+                <button type="submit">등록</button>
             </div>
             <style jsx>{`
                 form {
@@ -76,9 +107,30 @@ const registerBook = ()=>{
                     display : flex;
                     flex-direction : row;
                     height : 35px;
+                    flex : 1;
+                    margin-top : 10px;
+                }
+                label {
+                    flex : 1;
+                }
+                input {
+                    flex : 3;
+                }
+                select{
+                    flex : 3;
                 }
                 .img-btn{
                     flex : 1;
+                    margin : 10px;
+                    border : 1px solid #c4cfb0;
+                    background : #d8dfcb; 
+                    display : flex;
+                    align-items: center;
+                    justify-content : center;
+                }
+
+                .btn-div{
+                    margin : 10px;
                 }
             `}</style>
         </form>
