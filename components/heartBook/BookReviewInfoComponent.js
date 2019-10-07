c2f26822549533677c812bccd80cf17383d2a697
QsunA <ks.aum@theenm.com>
2019-10-07 18:50:53 +0900

    component add

--- components/heartBook/BookReviewInfoComponent.js
+++ components/heartBook/BookReviewInfoComponent.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
-const BookReviewInfoComponent = ()=>{
+const BookReviewInfoComponent = ({title, content})=>{
     return(
-        <div>
+        <div className="review-main">
             <img src='/static/img/no_img.png'/>
-            <div>
-                <p>title</p>
+            <div className="review-content">
+                <p>{title}</p>
+                <div >
+                    {content}
+                </div>
             </div>
+            <style jsx>{`
+                .review-main{
+                    display : flex;
+                    flex-direction : row;
+                    flex : 1;
+                }
+
+                img{
+                    flex : 1;
+                }
+
+                .review-content{
+                    display : flex;
+                    flex-direction : column;
+                    flex: 3;
+                }
+
+                p{
+                    text-decoration : bold;
+                }
+            `}</style>
         </div>
     );
 };
--- pages/myBook.js
+++ pages/myBook.js
@@ -1,15 +1,60 @@
 import BookImgListComponent from "../components/myBook/BookImgListComponent"
 import Link from "next/link";
+import { useCallback, useState, createRef, useRef } from "react";
 
 
 const myBook = ()=>{
+
+    const [file,setFile] = useState('');
+    const userFileName = useRef();
+
+    const onSubmit = useCallback((e)=>{
+        e.preventDefault();
+        if(file === null){
+            return;
+        }
+
+    },[]);
+
+    const onChange = useCallback((e) => {
+        setFile(e.target.files[0]);
+        userFileName.current.value = e.target.files[0].name;
+    },[]);    
+
     return(
-        <div>
-            <div>
+        <div className="mybook-main">
+            <div className="mybook-header">
                 <Link href="./registerBook"><button>책 등록하기</button></Link>
-                <button>엑셀로 책 등록하기</button>
+                <form onSubmit={onSubmit}>
+                    <input placeholder="선택된 파일" type="text" ref={userFileName}/>
+                    <label htmlFor="userfile">
+                        <input type="file" accept=".xlsx,xls,csv"
+                            onChange={onChange}
+                        />
+                    </label>
+                    <button type="submit">올리기</button>
+                </form>
             </div>
             <BookImgListComponent/>
+            <style jsx>{`
+                .mybook-main{
+                    display : flex;
+                    flex-direction:column;
+                }
+
+                .mybook-header{
+                    display : flex;
+                    flex-direction:row;
+                    height : 25px;
+                    width : 100%;
+                    justify-content : flex-end;
+                }
+                
+                form {
+                    display : flex;
+                    flex-direction:row;                    
+                }
+            `}</style>
         </div>
     )
 };
